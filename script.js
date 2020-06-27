ff3205aa547daa581617372916ee7251de9c1b06
SirPhenom <iphenom01@gmial.com>
2020-06-27 07:32:25 +0100

    Added the first modal

--- script.js
+++ script.js
@@ -18,55 +18,6 @@ submitBtn.onclick = () => {
   errorMsg.classList.remove("d-none");
 };
 
-// $(document).ready(function () {
-//   $(document).on("scroll", onScroll);
-
-//   //smoothscroll
-//   $('a[href^="#"]').on("click", function (e) {
-//     e.preventDefault();
-//     $(document).off("scroll");
-
-//     $("a").each(function () {
-//       $(this).removeClass("active");
-//     });
-//     $(this).addClass("active");
-
-//     var target = this.hash,
-//       navbar = target;
-//     $target = $(target);
-//     $("html, body")
-//       .stop()
-//       .animate(
-//         {
-//           scrollTop: $target.offset().top + 20,
-//         },
-//         500,
-//         "swing",
-//         function () {
-//           window.location.hash = target;
-//           $(document).on("scroll", onScroll);
-//         }
-//       );
-//   });
-// });
-
-// function onScroll(event) {
-//   var scrollPos = $(document).scrollTop();
-//   $("#navbarLinks a").each(function () {
-//     var currLink = $(this);
-//     var refElement = $(currLink.attr("href"));
-//     if (
-//       refElement.position().top <= scrollPos &&
-//       refElement.position().top + refElement.height() > scrollPos
-//     ) {
-//       $("#navbarLinks ul li a").removeClass("active");
-//       currLink.addClass("active");
-//     } else {
-//       currLink.removeClass("active");
-//     }
-//   });
-// }
-
 // Cache selectors
 var lastId,
   topMenu = $("#top-menu"),
@@ -122,3 +73,14 @@ $(window).scroll(function () {
       .addClass("border-rad");
   }
 });
+
+var wait = (id, time) => {
+  setTimeout(function () {
+    var tellBtn = document.getElementById(id);
+    tellBtn.classList.remove("d-none");
+  }, parseInt(time));
+};
+
+wait("hello", 1000);
+wait("display", 2000);
+wait("tell-me-more", 2000);
